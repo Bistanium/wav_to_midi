d78aed65864b065971f7ed198aca5f935075413e
Bistam <63497549+Bistanium@users.noreply.github.com>
2024-04-21 20:31:45 +0900

    Update main.py
    ノートの重複問題の修正
    メモリ使用量の削減
    
    代わりに音が少し籠るようになった
    良く言えば自然になった

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import numpy as np
 #midi化関数
 def data2midi(F, fs, N):
     sec = N / fs
-    count = 0
-    otolist = [35]
+    loopcount, beforenote, maxvolume = 0, 0, 0
     for i in range(1, int(N/2), 1):
         if abs(F.imag[i]/N*2) > 32: #音量の閾値
             if 66 < i/sec and i/sec < 11175	: #midiの範囲に収める
@@ -17,51 +16,51 @@ def data2midi(F, fs, N):
                 midinote = round(69 + log/0.025085832971998432934478241227, 1)
 
                 #音量計算
-                volume = (abs(F.imag[i]/N*2) ** (1.8/3))
+                volume = (abs(F.imag[i]/N*2) ** (1.8/3))*1.25
                 #音量調整
-                if midinote < 48:
+                if midinote < 56:
                     volume = volume * 0.8
-                    if midinote < 41:
+                    if midinote < 48:
                         volume = volume * 0.7
+                        if midinote < 41:
+                            volume = volume * 0.6
+
                 if midinote > 107:
                     volume = volume * 0.8
                     if midinote > 112:
                         volume = volume * 0.7
                         if midinote > 118:
                             volume = volume * 0.6
+
                 if volume > 127: volume = 127
 
-                otolist.append(int(round(midinote, 0)))
-                syosu, seisu = math.modf(midinote) #整数部分と小数部分の分離
-                syosu = round(syosu, 1)
-                if syosu == 0.6:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.7, 0)), channel=0, time=00))
-                elif syosu == 0.7:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.8, 0)), channel=0, time=00))
-                elif syosu == 0.8:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.9, 0)), channel=0, time=00))
-                elif syosu == 0.9:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.9, 0)), channel=0, time=00))
-                elif syosu == 0.0:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*1, 0)), channel=0, time=00))
-                elif syosu == 0.1:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.9, 0)), channel=0, time=00))
-                elif syosu == 0.2:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.9, 0)), channel=0, time=00))
-                elif syosu == 0.3:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.8, 0)), channel=0, time=00))
-                elif syosu == 0.4:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.7, 0)), channel=0, time=00))
-                elif syosu == 0.5:
-                    track0.append(Message('note_on', note=int(round(midinote, 0)), velocity=int(round(volume*0.6, 0)), channel=0, time=00))
+                loopcount += 1
+                if not beforenote == int(round(midinote, 0)):
+                    syosu, seisu = math.modf(beforenote) #整数と小数部分の分離
+                    syosu = round(syosu, 1)
 
-    for j in otolist:
-        count += 1
-        if count == 1:
+                    if syosu == 0.0:
+                        track.append(Message('note_on', note=beforenote, velocity=int(round(maxvolume*1, 0)), time=00))
+                    elif syosu == 0.9 or syosu == 0.8 or syosu == 0.1 or syosu == 0.2:
+                        track.append(Message('note_on', note=beforenote, velocity=int(round(maxvolume*0.9, 0)), time=00))
+                    elif syosu == 0.7 or syosu == 0.3:
+                        track.append(Message('note_on', note=beforenote, velocity=int(round(maxvolume*0.8, 0)), time=00))
+                    elif syosu == 0.6 or syosu == 0.4:
+                        track.append(Message('note_on', note=beforenote, velocity=int(round(maxvolume*0.7, 0)), time=00))
+                    elif syosu == 0.5:
+                        track.append(Message('note_on', note=beforenote, velocity=int(round(maxvolume*0.6, 0)), time=00))
+
+                    beforenote, maxvolume = int(round(midinote, 0)), volume
+                    loopcount = 0
+                else:
+                    if volume > maxvolume: maxvolume = volume
+
+    for j in range(35, 126):
+        if j == 35:
             soundtime = 240*sec
-            track0.append(Message('note_off', note=int(j), channel=0, time=int(round(soundtime, 0))))
+            track.append(Message('note_off', note=j, time=int(round(soundtime, 0))))
         else:
-            track0.append(Message('note_off', note=int(j), channel=0, time=0))
+            track.append(Message('note_off', note=j, time=0))
 
 
 # Wave読み込み
@@ -117,11 +116,12 @@ if __name__ == '__main__':
     
     # midi定義
     mid = MidiFile()
-    track0 = MidiTrack()
-    mid.tracks.append(track0)
+    track = MidiTrack()
+    mid.tracks.append(track)
  
     # Wav読み込み
     data_l,data_r = read_wav("test.wav")
+    del data_r
 
     # Wavの情報取得
     wi = info_wav("test.wav")
@@ -133,19 +133,22 @@ if __name__ == '__main__':
     audiospeed = 16 / 8
     miditempo = (480/(wi["fs"]/win_size))/audiospeed
     miditempo = round(480 * (round(miditempo, 0) / miditempo) - audiospeed, 2)
-    track0.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(miditempo)))
+    track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(miditempo)))
     
     # データ分割
     data_ls = audio_split(data_l, wi, win_size)
+    del data_l
 
     # FFT
     F_ls = []
     for dl in data_ls:
         F_ls.append(np.fft.fft(dl))
+    del data_ls
 
     # midi化
     for F_l in F_ls:
         data2midi(F_l, wi["fs"], len(F_l.imag))
+    del F_ls
 
     out_file = "test_wav_midi.mid"
     mid.save(out_file)
